34720564df1d9adde9ffe7472b20a5bd3f977f77
camelcdr <camel.cdr@gmail.com>
2024-04-29 05:37:16 +0800

    read traffic limit in data file

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
+	"github.com/tidwall/gjson"
 	"os"
 	"strings"
 )
 
-func readMysqlPwd(filepath string) string {
+func readMysqlPwd() string {
+	var dir = "/home/nokvm/go/src/node/bin/conf"
+	var filename = "app.conf"
+	filepath := dir + "/" + filename
 	file, err := os.Open(filepath)
 	if err != nil {
 		fmt.Println("打开配置文件失败：", err)
@@ -28,10 +33,46 @@ func readMysqlPwd(filepath string) string {
 	panic("获取密码失败")
 }
 
+func readInstanceTrafficLimits() map[string]string {
+	trafficLimits := make(map[string]string)
+	var filepath = "/home/nokvm/nokvm.data"
+	file, err := os.Open(filepath)
+	if err != nil {
+		return trafficLimits
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	json := scanner.Text()
+	guests := gjson.Get(json, "Guests")
+	guests.ForEach(func(key, value gjson.Result) bool {
+		name := gjson.Get(json, "name").String()
+		trafficLimit := gjson.Get(json, "traffic_limit").String()
+		trafficLimits[name] = trafficLimit
+		return true
+	})
+	return trafficLimits
+}
+
+func correctDb(passwd string) {
+	db, err := sql.Open("mysql", "username:"+passwd+"@tcp(127.0.0.1:3306)/node")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+}
+
 func main() {
-	var dir = "/home/nokvm/go/src/node/bin/conf"
-	var filename = "app.conf"
-	filepath := dir + "/" + filename
-	passwd := readMysqlPwd(filepath)
+	passwd := readMysqlPwd()
 	fmt.Println("当前的nokvm被控的数据库密码为：", passwd)
+
+	trafficLimits := readInstanceTrafficLimits()
+	count := 0
+	for name, traffic := range trafficLimits {
+		fmt.Println(count, "\tName:", name, "traffic:", traffic)
+		count++
+	}
+
+	//correctDb(passwd)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module fix_nokvm_traffic_overflow
 
 go 1.22
+
+require github.com/tidwall/gjson v1.17.1
+
+require (
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+)
